5130816bc75156489d675c40e89fa5bfc95cb0b0
[jsw324] <[jwalkow@tallience.com]>
2016-08-20 09:40:45 -0400

    updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,28 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
-  end
-  private
-    def article_params
-      params.require[:article] .permit(:title, :text)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 end
+
+private
+  def article_params
+    params.require(:article) .permit(:title, :text)
+  end
